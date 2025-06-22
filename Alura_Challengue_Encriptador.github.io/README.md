0a70168da738f6ba3bd451779fcd164f49001f57
Hack-Gamer001 <119648000+Hack-Gamer001@users.noreply.github.com>
2025-06-22 01:11:37 +0000

    Importando Encriptador de Alura como carpeta en Paginas_Web

---
+++ Alura_Challengue_Encriptador.github.io/README.md
@@ -0,0 +1,19 @@
+![concluido-es](https://github.com/Hack-Gamer001/Challengue-Encriptador/assets/119648000/18f6d3a2-4948-4ba8-9c8d-a3c068390439)
+
+
+![pruebas y finalizacion trello](https://github.com/Hack-Gamer001/Challengue-Encriptador/assets/119648000/f04c13b9-737f-4939-bfa3-260a38acfee4)
+
+
+Mensaje final
+
+![image](https://github.com/Hack-Gamer001/Challengue-Encriptador/assets/119648000/698556e6-bcf1-426c-b052-a2b8e9d1d795)
+
+
+
+
+video demostracion
+
+
+
+https://github.com/Hack-Gamer001/Alura-Challengue-Encriptador/assets/119648000/0bbc2186-1769-47fb-a684-31b5306ff149
+
---
+++ Alura_Challengue_Encriptador.github.io/script.js
@@ -0,0 +1,34 @@
+function encryptText() {
+    const inputText = document.getElementById('inputText').value;
+    const encryptedText = inputText
+        .replace(/e/g, 'enter')
+        .replace(/i/g, 'imes')
+        .replace(/a/g, 'ai')
+        .replace(/o/g, 'ober')
+        .replace(/u/g, 'ufat');
+    document.getElementById('outputText').value = encryptedText;
+}
+
+function decryptText() {
+    const inputText = document.getElementById('inputText').value;
+    const decryptedText = inputText
+        .replace(/enter/g, 'e')
+        .replace(/imes/g, 'i')
+        .replace(/ai/g, 'a')
+        .replace(/ober/g, 'o')
+        .replace(/ufat/g, 'u');
+    document.getElementById('outputText').value = decryptedText;
+}
+
+function copyText() {
+    const outputText = document.getElementById('outputText');
+    outputText.select();
+    outputText.setSelectionRange(0, 99999); // Para dispositivos móviles
+    document.execCommand('copy');
+    alert('Texto encriptado copiado');
+}
+
+function clearText() {
+    document.getElementById('inputText').value = '';
+    document.getElementById('outputText').value = '';
+}
